188b43daa64a7845b2e4042d99ab5c2eed9f7631
kabirolawore <horlaworey@gmail.com>
2023-08-27 21:58:08 +0100

    7.17 project added

---
+++ 7.17 Navigtion/bloglist-frontend/src/App.js
@@ -0,0 +1,85 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import blogService from "./services/blogs";
+import Notification from "./components/Notification";
+import { setBlogs } from "./reducers/blogReducer";
+import LoginForm from "./components/LoginForm";
+import { getUser } from "./reducers/userReducer";
+import Blogs from "./components/Blogs";
+import Users from "./components/Users";
+// import ErrorPage from "./components/ErrorPage";
+
+import { Routes, Route, Link } from "react-router-dom";
+import User from "./components/User";
+import Blog from "./components/Blog";
+
+const App = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
+
+  useEffect(() => {
+    blogService.getAll().then((blogs) => dispatch(setBlogs(blogs)));
+  }, []);
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedBlogAppUser");
+
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      dispatch(getUser(user));
+      blogService.setToken(user.token);
+    }
+  }, []);
+
+  const handleLogout = () => {
+    // remove user from localStorage and log out the user
+    window.localStorage.removeItem("loggedBlogAppUser");
+    // Redirect the user to the login page
+    window.location.href = "/";
+  };
+
+  const padding = {
+    padding: 3,
+  };
+
+  return (
+    <div className="container">
+      <div>{!user?.name && <LoginForm />}</div>
+
+      <div>
+        {user?.name && (
+          <div>
+            <h1>blogs</h1>
+            <Notification />
+            <div>
+              <div>
+                <Link style={padding} to="/blogs">
+                  blogs
+                </Link>
+                <Link style={padding} to="/users">
+                  users
+                </Link>{" "}
+                {user?.name} logged in{" "}
+                <button onClick={handleLogout}>logout</button>
+              </div>
+            </div>
+            <br />
+            <Routes>
+              <Route path="/" element={<Blogs user={user} />} />
+              <Route path="/users" element={<Users />} />
+              <Route path="/users/:id" element={<User />} />
+              <Route path="/blogs" element={<Blogs user={user} />} />
+              <Route path="/blogs/:id" element={<Blog />} />
+            </Routes>
+          </div>
+        )}
+      </div>
+      <footer>
+        <br />
+        <em>Blog app, Department of Computer Science 2023</em>
+      </footer>
+    </div>
+  );
+};
+
+export default App;
---
+++ 7.17 Navigtion/bloglist-frontend/src/components/Blog.js
@@ -0,0 +1,91 @@
+import { useDispatch, useSelector } from "react-redux";
+import { increaseBlogLike, deleteBlog } from "../reducers/blogReducer";
+import blogService from "../services/blogs";
+import { useParams, useNavigate } from "react-router-dom";
+
+//
+const Blog = () => {
+  //
+  const id = useParams().id;
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+  const [blog] = useSelector((state) => {
+    return [...state.blogs].filter((blog) => blog.id === id);
+  });
+  const user = useSelector((state) => state.user.username);
+
+  // console.log("blogs", blog);
+  // console.log("user", user);
+
+  const buttonStyle = {
+    backgroundColor: "#4dabf7",
+    borderRadius: "10%",
+    paddingLeft: "5px",
+    paddingRight: "5px",
+  };
+
+  const deleteStyle = {
+    backgroundColor: "red",
+    borderRadius: "10%",
+    paddingLeft: "5px",
+    paddingRight: "5px",
+  };
+
+  const handleLikes = async () => {
+    const updatedBlog = {
+      ...blog,
+      likes: blog?.likes + 1,
+    };
+
+    try {
+      await blogService.update(blog.id, updatedBlog);
+      dispatch(increaseBlogLike(updatedBlog.id));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleDelete = () => {
+    if (window.confirm(`Remove blog ${blog.title}? by ${blog.author}`))
+      try {
+        blogService.del(blog.id) && dispatch(deleteBlog(blog.id));
+        navigate("/blogs");
+      } catch (error) {
+        console.log(error);
+      }
+  };
+
+  return (
+    <div className="blog">
+      <div>
+        <h2>
+          {blog?.title} by {blog?.author}
+        </h2>
+        <br />
+        <a href={blog?.url}>{blog?.url}</a>
+        <div>
+          {blog?.likes} likes{" "}
+          <button style={buttonStyle} onClick={() => handleLikes()}>
+            like
+          </button>
+          <p>
+            added by <strong>{blog?.user.name}</strong>
+          </p>
+          <br />
+          <p>
+            {blog?.user.username === user ? (
+              <button onClick={() => handleDelete()} style={deleteStyle}>
+                delete
+              </button>
+            ) : (
+              ""
+            )}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
